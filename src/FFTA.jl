e6bd067c9b5b062cac612791f48a20a49a149eec
Danny Sharp <dannys4@mit.edu>
2021-08-08 18:20:02 -0400

    Move callgraph out

--- src/FFTA.jl
+++ src/FFTA.jl
@@ -4,6 +4,62 @@ using Primes, DocStringExtensions, LoopVectorization
 import Base: getindex
 export fft, bfft
 
+include("callgraph.jl")
 include("algos.jl")
+
+function fft(x::AbstractVector{T}) where {T}
+    y = similar(x)
+    g = CallGraph{T}(length(x))
+    fft!(y, x, Val(FFT_FORWARD), g[1].type, g, 1)
+    y
+end
+
+function fft(x::AbstractVector{T}) where {T <: Real}
+    y = similar(x, Complex{T})
+    g = CallGraph{Complex{T}}(length(x))
+    fft!(y, x, Val(FFT_FORWARD), g[1].type, g, 1)
+    y
+end
+
+function fft(x::AbstractMatrix{T}) where {T}
+    M,N = size(x)
+    y1 = similar(x)
+    y2 = similar(x)
+    g1 = CallGraph{T}(size(x,1))
+    g2 = CallGraph{T}(size(x,2))
+
+    for k in 1:N
+        @views fft!(y1[:,k],  x[:,k], Val(FFT_FORWARD), g1[1].type, g1, 1)
+    end
+
+    for k in 1:M
+        @views fft!(y2[k,:], y1[k,:], Val(FFT_FORWARD), g2[1].type, g2, 1)
+    end
+    y2
+end
+
+function bfft(x::AbstractVector{T}) where {T}
+    y = similar(x)
+    g = CallGraph{T}(length(x))
+    fft!(y, x, Val(FFT_BACKWARD), g[1].type, g, 1)
+    y
+end
+
+function bfft(x::AbstractMatrix{T}) where {T}
+    M,N = size(x)
+    y1 = similar(x)
+    y2 = similar(x)
+    g1 = CallGraph{T}(size(x,1))
+    g2 = CallGraph{T}(size(x,2))
+
+    for k in 1:N
+        @views fft!(y1[:,k],  x[:,k], Val(FFT_BACKWARD), g1[1].type, g1, 1)
+    end
+
+    for k in 1:M
+        @views fft!(y2[k,:], y1[k,:], Val(FFT_BACKWARD), g2[1].type, g2, 1)
+    end
+    y2
+end
 
 end
---
+++ src/callgraph.jl
@@ -0,0 +1,98 @@
+@enum Direction FFT_FORWARD FFT_BACKWARD
+abstract type AbstractFFTType end
+
+# Represents a Composite Cooley-Tukey FFT
+struct CompositeFFT <: AbstractFFTType end
+
+# Represents a Radix-2 Cooley-Tukey FFT
+struct Pow2FFT <: AbstractFFTType end
+
+# Represents an O(N²) DFT
+struct DFT <: AbstractFFTType end
+
+"""
+$(TYPEDSIGNATURES)
+Node of a call graph
+
+# Arguments
+`left::Int`- Offset to the left child node
+`right::Int`- Offset to the right child node
+`type::AbstractFFTType`- Object representing the type of FFT
+`sz::Int`- Size of this FFT
+
+# Examples
+```julia
+julia> CallGraphNode(0, 0, Pow2FFT(), 8)
+```
+"""
+struct CallGraphNode
+    left::Int
+    right::Int
+    type::AbstractFFTType
+    sz::Int
+end
+
+"""
+$(TYPEDSIGNATURES)
+Object representing a graph of FFT Calls
+
+# Arguments
+`nodes::Vector{CallGraphNode}`- Nodes keeping track of the graph
+`workspace::Vector{Vector{T}}`- Preallocated Workspace
+
+# Examples
+```julia
+julia> CallGraph{ComplexF64}(CallGraphNode[], Vector{T}[])
+```
+"""
+struct CallGraph{T<:Complex}
+    nodes::Vector{CallGraphNode}
+    workspace::Vector{Vector{T}}
+end
+
+# Get the node in the graph at index i
+Base.getindex(g::CallGraph{T}, i::Int) where {T} = g.nodes[i]
+
+# Get the left child of the node at index `i`
+leftNode(g::CallGraph, i::Int) = g[i+g[i].left]
+
+# Get the right child of the node at index `i`
+rightNode(g::CallGraph, i::Int) = g[i+g[i].right]
+
+# Recursively instantiate a set of `CallGraphNode`s
+function CallGraphNode!(nodes::Vector{CallGraphNode}, N::Int, workspace::Vector{Vector{T}})::Int where {T}
+    facs = factor(N)
+    Ns = [first(x) for x in collect(facs) for _ in 1:last(x)]
+    if length(Ns) == 1 || Ns[end] == 2
+        push!(workspace, T[])
+        push!(nodes, CallGraphNode(0,0,Ns[end] == 2 ? Pow2FFT() : DFT(),N))
+        return 1
+    end
+
+    if Ns[1] == 2
+        N1 = prod(Ns[Ns .== 2])
+    else
+        # Greedy search for closest factor of N to sqrt(N)
+        Nsqrt = sqrt(N)
+        N_cp = cumprod(Ns[end:-1:1])[end:-1:1]
+        N_prox = abs.(N_cp .- Nsqrt)
+        _,N1_idx = findmin(N_prox)
+        N1 = N_cp[N1_idx]
+    end
+    N2 = N ÷ N1
+    push!(nodes, CallGraphNode(0,0,DFT(),N))
+    sz = length(nodes)
+    push!(workspace, Vector{T}(undef, N))
+    left_len = CallGraphNode!(nodes, N1, workspace)
+    right_len = CallGraphNode!(nodes, N2, workspace)
+    nodes[sz] = CallGraphNode(1, 1 + left_len, CompositeFFT(), N)
+    return 1 + left_len + right_len
+end
+
+# Instantiate a CallGraph from a number `N`
+function CallGraph{T}(N::Int) where {T}
+    nodes = CallGraphNode[]
+    workspace = Vector{Vector{T}}()
+    CallGraphNode!(nodes, N, workspace)
+    CallGraph(nodes, workspace)
+end
