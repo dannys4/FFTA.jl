247aa244e143873bad983a852520c5270e85adfe
Danny Sharp <dannys4@mit.edu>
2022-01-02 16:53:16 -0500

    Fixed 2d real forward test

--- test/twodim/real_forward.jl
+++ test/twodim/real_forward.jl
@@ -3,7 +3,7 @@ test_nums = [8, 11, 15, 16, 27, 100]
 @testset " forward" begin 
     for N in test_nums
         x = ones(Float64, N, N)
-        y = fft(x)
+        y = rfft(x)
         y_ref = 0*y
         y_ref[1] = length(x)
         @test y ≈ y_ref
