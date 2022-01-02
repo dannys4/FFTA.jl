d9c65c33790fed12a34ca337c4bc625fa84cc6a6
Danny Sharp <dannys4@mit.edu>
2022-01-02 16:49:50 -0500

    Fixed onedim tests

--- test/onedim/real_backward.jl
+++ test/onedim/real_backward.jl
@@ -1,11 +1,14 @@
-using FFTA, Test
+using FFTA, Test, LinearAlgebra
 test_nums = [8, 11, 15, 16, 27, 100]
 @testset "backward" begin
     for N in test_nums
         x = ones(Float64, N)
-        y = bfft(x)
+        y = brfft(x, 2*(N-1))
         y_ref = 0*y
-        y_ref[1] = N
+        y_ref[1] = 2*(N-1)
+        if !isapprox(y_ref, y, atol=1e-12)
+            println(norm(y_ref - y))
+        end
         @test y_ref ≈ y atol=1e-12
     end
 end
